2b7c4857efd130da1f6e0bce973aad78c0af0d34
Fabio Junior Ribeiro <rib3iro@live.com>
2020-06-25 16:12:21 -0300

    first commit

--- gnome.sh
+++ gnome.sh
@@ -10,11 +10,11 @@ fim='\e[m'
 seta='\e[32;1m==>\e[m'
 
 # Funções ------------------------------------------------------------
-driver_virtmanager(){
+virtmanager(){
     echo $pass_user | sudo -S pacman -S xf86-video-qxl --noconfirm
 }
 
-driver_nvidia(){
+nvidia(){
     clear
     echo -e "${seta} ${vrm}O driver da nvidia está em manutenção!${fim}"
     # echo $pass_user | sudo -S pacman -S nvidia nvidia-utils intel-ucode --noconfirm
@@ -22,16 +22,13 @@ driver_nvidia(){
     clear
 }
 
-instalar_gdm(){
+gdm(){
     echo $pass_user | sudo -S pacman -S gdm --noconfirm
-}
-
-iniciar_gdm(){
     echo $pass_user | sudo -S systemctl enable gdm
     echo $pass_user | sudo -S systemctl start gdm
 }
 
-iniciar_bluez(){
+bluez(){
     echo $pass_user | sudo -S systemctl status bluetooth
     echo $pass_user | sudo -S systemctl enable bluetooth
     echo $pass_user | sudo -S systemctl start bluetooth
@@ -58,34 +55,31 @@ clear
 if [ "$resposta" -eq 1 ]; then
     echo -e "${seta} ${azl}Iniciando instalação do driver para virt-manager${fim}"
     sleep 2s
-    driver_virtmanager
+    virtmanager
     clear
 elif [ "$resposta" -eq 2 ]; then
     echo -e "${seta} ${azl}Iniciando instalação do driver para nvidia${fim}"
     sleep 2s
-    driver_nvidia
+    nvidia
     clear
 else
     echo -e "${seta} ${vrm}Resposta inválida!${fim}"
     exit 1
 fi
 
-echo -e "${seta} ${azl}Instalando o gnome${fim}"
-sleep 2s
-echo $pass_user | sudo -S pacman -S gnome --noconfirm
-clear
-
+# Instalando pacotes ----------------------------------------------------
 echo -e "${seta} ${azl}Instalando pacotes necessários${fim}"
 sleep 2s
-
-echo $pass_user | sudo -S pacman -S archlinux-keyring archlinux-wallpaper bash-completion bluez bluez-utils chrome-gnome-shell cmatrix cronie dialog gimp gnome-keyring gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db neofetch pass powerline-fonts rsync speedtest-cli totem transmission-gtk ttf-hack gnu-free-fonts ttf-dejavu ttf-nerd-fonts-symbols ufw unrar xdg-user-dirs xdg-utils xf86-input-synaptics xcursor-vanilla-dmz-aa xclip youtube-dl lolcat --noconfirm
+echo $pass_user | sudo -S pacman -S gnome gedit archlinux-keyring os-prober mtools dosfstools archlinux-wallpaper bash-completion bluez bluez-utils chrome-gnome-shell cmatrix cronie dialog gimp gnome-keyring gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db neofetch pass powerline-fonts rsync speedtest-cli totem transmission-gtk ttf-hack gnu-free-fonts ttf-dejavu ttf-nerd-fonts-symbols ufw unrar xdg-user-dirs xdg-utils xf86-input-synaptics xcursor-vanilla-dmz-aa xclip youtube-dl lolcat --noconfirm
 clear
 
+# Desinstalando pacotes -------------------------------------------------
 echo -e "${seta} ${vrm}Desinstalando pacotes desnecessários${fim}"
 sleep 2s
-echo $pass_user | sudo -S pacman -R malcontent gnome-maps gnome-boxes gnome-books gnome-contacts gnome-music gnome-software gnome-terminal --noconfirm
+echo $pass_user | sudo -S pacman -R epiphany malcontent gnome-maps gnome-boxes gnome-books gnome-contacts gnome-music gnome-software gnome-terminal --noconfirm
 clear
 
+# YAY -------------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando o yay${fim}"
 sleep 2s
 git clone https://aur.archlinux.org/yay.git
@@ -93,16 +87,7 @@ cd yay
 makepkg -si --noconfirm
 clear
 
-# Gedit -----------------------------------------------------------
-echo -e "${seta} ${azl}Instalando o gedit com tema dracula${fim}"
-sleep 2s
-echo $pass_user | sudo -S pacman -S gedit
-wget https://raw.githubusercontent.com/dracula/gedit/master/dracula.xml
-mkdir -p $HOME/.local/share/gedit/styles
-mv dracula.xml $HOME/.local/share/gedit/styles/
-clear
-
-# Virt-manager ----------------------------------------------------
+# Virt-manager ----------------------------------------------------------
 echo -e "${seta} ${azl}Instalando o virt-manager${fim}"
 sleep 2s
 echo $pass_user | sudo -S pacman -S qemu virt-manager dnsmasq libvirt ebtables bridge-utils ovmf spice-vdagent --noconfirm
@@ -123,8 +108,8 @@ echo -e "${seta} ${azl}Configurando a vrme do virt-manager para iniciar automát
 sleep 2s
 echo $pass_user | sudo -S virsh net-autostart --network default
 clear
-# Virt-manager ----------------------------------------------------
 
+# Timeshift ------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando o timeshift${fim}"
 sleep 2s
 yay -S timeshift --noconfirm
@@ -135,7 +120,7 @@ sleep 2s
 yay -S lollypop-stable-git kid3-cli --noconfirm
 clear
 
-# Hackerman -------------------------------------------------------
+# Hackerman ------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando aircrack-ng e usbutils${fim}"
 sleep 2s
 echo $pass_user | sudo -S pacman -S aircrack-ng usbutils tcpdump --noconfirm
@@ -145,13 +130,14 @@ echo -e "${seta} ${azl}Instalando o crunch${fim}"
 sleep 2s
 yay -S crunch --noconfirm
 clear
-# Hackerman -------------------------------------------------------
 
+# Firmwares -------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando os${fim} ${amr}firmwares warnigs${fim} ${azl}do archlinux${fim}"
 sleep 2s
 yay -S aic94xx-firmware wd719x-firmware --noconfirm
 clear
 
+# Gnome-terminal-transparency -------------------------------------------
 echo -e "${seta} ${azl}Instalando o gnome-terminal-transparency${fim}"
 sleep 2s
 yay -S gnome-terminal-transparency --noconfirm
@@ -160,26 +146,31 @@ yay -S gnome-terminal-transparency --noconfirm
 #./install.sh
 clear
 
+# Mint-icons ------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando o mint-icons${fim}"
 sleep 2s
 yay -S mint-y-icons --noconfirm
 clear
 
+# Debtap ----------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando o debtap${fim}"
 sleep 2s
 yay -S debtap --noconfirm
 clear
 
+# Google-chrome ---------------------------------------------------------
 echo -e "${seta} ${azl}Instalando o google-chrome${fim}"
 sleep 2s
 yay -S google-chrome --noconfirm
 clear
 
+# Spotify ---------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando spotify${fim}"
 sleep 2s
 yay -S spotify --noconfirm
 clear
 
+# Fontes ----------------------------------------------------------------
 echo -e "${seta} ${azl}Instalando as fontes${fim}"
 sleep 2s
 yay -S ttf-ms-fonts --noconfirm
@@ -187,22 +178,21 @@ yay -S ttf-roboto --noconfirm
 yay -S ttf-ubuntu-font-family --noconfirm
 clear
 
-echo -e "${seta} ${azl}Iniciando o bluez${fim}"
-sleep 2s
-iniciar_bluez
-clear
-
+# xdg-user-dirs ---------------------------------------------------------
 echo -e "${seta} ${azl}Iniciando o xdg-update${fim}"
 sleep 2s
 xdg-user-dirs-update
 clear
 
-echo -e "${seta} ${azl}Instalando o gdm${fim}"
+# Bluez -----------------------------------------------------------------
+echo -e "${seta} ${azl}Iniciando o bluez${fim}"
 sleep 2s
-instalar_gdm 
+bluez
 clear
 
-echo -e "${seta} ${azl}Iniciando o serviço do gdm${fim}"
+# GDM -------------------------------------------------------------------
+echo -e "${seta} ${azl}Instalando o gdm${fim}"
 sleep 2s
-iniciar_gdm
+gdm 
+clear
 
