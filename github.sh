d500c6f0f8ff77568ae96726511f0337d72d19c0
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2020-10-22 23:38:26 -0300

    Add files via upload

---
+++ github.sh
@@ -0,0 +1,20 @@
+echo "baixando pacote zshell"
+sleep 2
+git clone https://github.com/frib3iro/zshell
+clear
+echo "baixando pacote archlinux"
+sleep 2
+git clone https://github.com/frib3iro/archlinux
+clear
+echo "baixando pacote gnome"
+sleep 2
+git clone https://github.com/frib3iro/gnome
+clear
+echo "baixando pacote vbz"
+sleep 2
+//git clone https://github.com/frib3iro/vbz
+clear
+echo "baixando pacote plymouth"
+sleep 2
+git clone https://github.com/frib3iro/plymouth
+clear
