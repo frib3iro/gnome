63c6da02950ae10f105d4525ea15891281f9da59
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2020-11-19 11:48:00 -0300

    Add files via upload

--- gnome.sh
+++ gnome.sh
@@ -72,7 +72,7 @@ fi
 # Instalando pacotes ----------------------------------------------------
 echo -e "${seta} ${ciano}Instalando pacotes necessários${fim}"
 sleep 2s
-echo $pass_user | sudo -S pacman -S arch-wiki-docs arch-wiki-lite alsa-utils grub-customizer archlinux-keyring archlinux-wallpaper bash-completion bluez bluez-utils chrome-gnome-shell cmatrix cronie cups dialog dosfstools efibootmgr git gedit gimp gnome gnome-keyring gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db mtools neofetch os-prober pass rsync speedtest-cli totem ufw unrar xclip xcursor-vanilla-dmz-aa xdg-user-dirs xdg-utils xf86-input-synaptics youtube-dl pulseaudio pulseaudio-bluetooth vim xfce4-terminal wget --noconfirm
+echo $pass_user | sudo -S pacman -S arch-wiki-docs arch-wiki-lite alsa-utils grub-customizer archlinux-keyring archlinux-wallpaper bash-completion bluez bluez-utils chrome-gnome-shell cmatrix cronie cups dialog dosfstools efibootmgr git gedit gimp gnome gnome-keyring gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db man-pages-pt_br mtools neofetch os-prober pass rsync speedtest-cli totem ufw unrar xclip xcursor-vanilla-dmz-aa xdg-user-dirs xdg-utils xf86-input-synaptics youtube-dl pulseaudio pulseaudio-bluetooth vim xfce4-terminal wget --noconfirm
 clear
 
 # Fontes ----------------------------------------------------------------
