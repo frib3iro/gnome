c8b170a7f0d5e777bc767532d56306d22a018def
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2021-01-16 14:08:17 -0300

    Add files via upload

--- gnome.sh
+++ gnome.sh
@@ -8,7 +8,7 @@
 # Data          : 11/12/2020
 # Licença       : GNU/GPL v3.0
 #----------------------------------------------------------------------
-# Uso : ./gnome
+# Uso           : ./gnome
 #----------------------------------------------------------------------
 # Cores usadas no script
 R='\033[0;31m'
@@ -20,10 +20,9 @@ F='\033[0m'
 # Seta utilizada no inicio das frases
 S='\e[32;1m[+]\e[m'
 #----------------------------------------------------------------------
-
 clear
 # Tela de boas vindas
-echo -e "${S} ${C}Bem vindo a instalação do gnome${F}"
+echo -e "${S} ${C}Bem vindo a instalação do gnome!${F}"
 sleep 1
 
 # Atualizando os espelhos
@@ -36,17 +35,17 @@ sudo pacman -Syu --noconfirm
 echo
 echo -e "${S} ${C}Instalando gnome...${F}"
 sleep 1
-sudo pacman -S gnome --noconfirm
+sudo pacman -S baobab cheese eog evince file-roller gdm gedit gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-contacts gnome-control-center gnome-disk-utility gnome-documents gnome-font-viewer gnome-getting-started-docs gnome-keyring gnome-logs gnome-maps gnome-menus gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-software gnome-system-monitor gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects gnome-weather grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mutter nautilus networkmanager orca rygel sushi totem tracker tracker-miners tracker3 tracker3-miners vino xdg-user-dirs-gtk yelp simple-scan --noconfirm
 
 # Instalando pacotes
 echo
-echo -e "${S} ${C}Instalando pacotes necessários${F}"
+echo -e "${S} ${C}Instalando pacotes...${F}"
 sleep 1
 sudo pacman -S arch-wiki-docs arch-wiki-lite alsa-utils dconf-editor gnome-passwordsafe gnome-sound-recorder gparted grub-customizer archlinux-keyring archlinux-wallpaper bash-completion bluez bluez-utils chrome-gnome-shell cmatrix cronie cups dialog dosfstools efibootmgr git gedit-plugins gimp gnome-tweaks gnupg gst-libav gufw htop libreoffice libreoffice-fresh-pt-br lolcat man-db man-pages-pt_br mesa-demos mtools neofetch os-prober pass rsync qbittorrent speedtest-cli ufw unrar xclip xcursor-vanilla-dmz-aa xdg-utils youtube-dl pulseaudio pulseaudio-bluetooth lollypop --noconfirm
 
 # YAY
 echo
-echo -e "${S} ${C}Instalando o yay${F}"
+echo -e "${S} ${C}Instalando o yay......${F}"
 sleep 1
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -54,85 +53,92 @@ makepkg -si --noconfirm
 
 # Firmwares
 echo
-echo -e "${S} ${C}Instalando os${F} ${Y}firmwares warnigs${F} ${C}do archlinux${F}"
+echo -e "${S} ${C}Instalando os${F} ${R}firmwares warnigs......${F}"
 sleep 1
 yay -S aic94xx-firmware wd719x-firmware upd72020x-fw --noconfirm
 
 # Mintstick
 echo
-echo -e "${S} ${C}Instalando o Mintstick${F}"
+echo -e "${S} ${C}Instalando o Mintstick...${F}"
 sleep 1
 yay -S mintstick --noconfirm
 
 # Downgrade
 echo
-echo -e "${S} ${C}Instalando o downgrade${F}"
+echo -e "${S} ${C}Instalando o downgrade...${F}"
+sleep 1
 yay -S downgrade --noconfirm
 
 # Timeshift
 echo
-echo -e "${S} ${C}Instalando o timeshift${F}"
+echo -e "${S} ${C}Instalando o timeshift...${F}"
 sleep 1
 yay -S timeshift --noconfirm
 
 # Hackerman
 echo
-echo -e "${S} ${C}Instalando Pacotes Hacker${F}"
+echo -e "${S} ${C}Instalando Pacotes Hacker...${F}"
 sleep 1
 sudo pacman -S aircrack-ng usbutils tcpdump wifite --noconfirm
 yay -S crunch --noconfirm
 
+# Gnome-terminal-transparency
+echo
+echo -e "${S} ${C}Instalando o gnome-terminal-transparency...${F}"
+sleep 1
+yay -S gnome-terminal-transparency --noconfirm
+
 # Cava
 echo
-echo -e "${S} ${C}Instalando o cava${F}"
+echo -e "${S} ${C}Instalando o cava...${F}"
 sleep 1
 yay -S cava --noconfirm
 
 # Ventoy
 echo
-echo -e "${S} ${C}Instalando o ventoy${F}"
+echo -e "${S} ${C}Instalando o ventoy...${F}"
 sleep 1
 yay -S ventoy-bin --noconfirm
 
 # Mint-icons
 echo
-echo -e "${S} ${C}Instalando o mint-icons${F}"
+echo -e "${S} ${C}Instalando o mint-icons...${F}"
 sleep 1
 yay -S mint-y-icons --noconfirm
 
 # Sardi-icons
 echo
-echo -e "${S} ${C}Instalando o sardi-icons${F}"
+echo -e "${S} ${C}Instalando o sardi-icons...${F}"
 sleep 1
 yay -S sardi-icons --noconfirm
 
 # Ant-dracula-theme-git
 echo
-echo -e "${S} ${C}Instalando o ant-dracula-theme-git${F}"
+echo -e "${S} ${C}Instalando o tema ant-dracula-theme-git...${F}"
 sleep 1
 yay -S ant-dracula-theme-git --noconfirm
 
 # Debtap
 echo
-echo -e "${S} ${C}Instalando o debtap${F}"
+echo -e "${S} ${C}Instalando o debtap...${F}"
 sleep 1
 yay -S debtap --noconfirm
 
 # Google-chrome
 echo
-echo -e "${S} ${C}Instalando o google-chrome${F}"
+echo -e "${S} ${C}Instalando o google-chrome...${F}"
 sleep 1
 yay -S google-chrome --noconfirm
 
 # Spotify
 echo
-echo -e "${S} ${C}Instalando o Spotify${F}"
+echo -e "${S} ${C}Instalando o Spotify...${F}"
 sleep 1
 yay -S spotify --noconfirm
 
 # Fontes
 echo
-echo -e "${S} ${C}Instalando as fontes${F}"
+echo -e "${S} ${C}Instalando as fontes...${F}"
 sleep 1
 sudo pacman -S ttf-fantasque-sans-mono ttf-jetbrains-mono ttf-dejavu ttf-hack --noconfirm
 yay -S ttf-ms-fonts --noconfirm
@@ -141,65 +147,49 @@ yay -S ttf-ubuntu-font-family --noconfirm
 
 # Tema dracula no gedit
 echo
-echo -e "${S} ${C}Baixando e instalando o tema Dracula no gedit${F}"
+echo -e "${S} ${C}Baixando e instalando o tema Dracula no gedit...${F}"
 sleep 1
-echo -e "${S} ${C}Baixando o tema Dracula para o gedit${F}"
 wget https://raw.githubusercontent.com/dracula/gedit/master/dracula.xml
-echo -e "${S} ${C}Verificando se o diretório styles existe${F}"
-
+sleep 1
 if [ -d "/home/fabio/.local/share/gedit/styles" ]
 then
-    echo -e "${S} ${Y}O diretório styles existe, apenas movendo o tema${F}"
-    sleep 1
     mv dracula.xml /home/fabio/.local/share/gedit/styles/
 else
-    echo -e "${S} ${Y}Criando o diretório styles e movendo o tema${F}"
-    sleep 1
     mkdir -p /home/fabio/.local/share/gedit/styles
     mv dracula.xml /home/fabio/.local/share/gedit/styles/
-fi
-echo -e "${S} ${G}Tema instalado com sucesso!${F}"
+fi  
+echo
+echo -e "${S} ${G}Tema instalado com sucesso!...${F}"
+sleep 1
 
 # Instalando o virt-manager
 echo
-echo -e "${S} ${C}Instalando o virt-manager${F}"
+echo -e "${S} ${C}Instalando o virt-manager...${F}"
+sleep 1
 sudo pacman -S qemu qemu-arch-extra virt-manager ovmf bridge-utils dnsmasq vde2 bridge-utils \
 openbsd-netcat ebtables iptables --noconfirm
-
-echo -e "${S} ${C}Instalando o libguestfs${F}"
 yay -S libguestfs --noconfirm
-
-echo -e "${S} ${C}Inicializando o serviço libvirt${F}"
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
-
-echo -e "${S} ${C}Habilitando o grupo libvirt para unix_sock_group${F}"
 sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/' /etc/libvirt/libvirtd.conf
-
-echo -e "${S} ${C}Habilitando leitura e escrita para o grupo libvirt${F}"
 sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/' /etc/libvirt/libvirtd.conf
-
-echo -e "${S} ${C}Adicionando o usuário ao grupo libvirt${F}"
 sudo usermod -a -G libvirt $(whoami)
-
-echo -e "${S} ${C}Reinicializando o serviço${F}"
 sudo systemctl restart libvirtd.service
-
-echo -e "${S} ${C}Configurando o libvirt para inicializar automaticamente o serviço de rede${F}"
 sudo virsh net-start default
 sudo virsh net-autostart --network default
 echo
-echo -e "${S} ${G}instalação do virt-manager concluída!${F}"
+echo -e "${S} ${G}Instalação do virt-manager concluída!...${F}"
+sleep 1
 
 # xdg-user-dirs
 echo
-echo -e "${S} ${C}Iniciando o xdg-update${F}"
+echo -e "${S} ${C}Iniciando o xdg-update...${F}"
 sleep 1
 xdg-user-dirs-update
 
 # Bluez
 echo
-echo -e "${S} ${C}Iniciando o bluez${F}"
+echo -e "${S} ${C}Iniciando o bluez...${F}"
 sleep 1
 sudo systemctl enable bluetooth
 sudo systemctl start bluetooth
@@ -207,7 +197,7 @@ sudo systemctl enable org.cups.cupsd
 
 # Gnome Display Manager
 echo
-echo -e "${S} ${C}Instalando o Gnome Display Manager${F}"
+echo -e "${S} ${C}Instalando o Gnome Display Manager...${F}"
 sleep 1
 sudo systemctl enable gdm.service
 sudo systemctl start gdm.service
